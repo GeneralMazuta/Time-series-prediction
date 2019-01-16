522ed1660e5498e8ac46bf3a5cdf1179bfa49e21
RomanVysokov <45870044+RomanVysokov@users.noreply.github.com>
2019-01-16 16:06:02 +0300

    Update TimeSeriesForecasting in R.R

--- TimeSeriesForecasting in R.R
+++ TimeSeriesForecasting in R.R
@@ -1,8 +1,9 @@
-#Присоединяемся к заранее созданному датасету в SQL Server, используя пакет RODBC, и записываем в датафрейм
+
+#РџСЂРёСЃРѕРµРґРёРЅСЏРµРјСЃСЏ Рє Р·Р°СЂР°РЅРµРµ СЃРѕР·РґР°РЅРЅРѕРјСѓ РґР°С‚Р°СЃРµС‚Сѓ РІ SQL Server, РёСЃРїРѕР»СЊР·СѓСЏ РїР°РєРµС‚ RODBC, Рё Р·Р°РїРёСЃС‹РІР°РµРј РІ РґР°С‚Р°С„СЂРµР№Рј
 library("RODBC")
 dbconnection <- odbcDriverConnect("Driver=ODBC Driver 11 for SQL Server;Server=****; Database=****;Uid=; Pwd=; trusted_connection=yes")
 Groupped_Fact_Data_44FZ_by_month<- sqlQuery(dbconnection,paste("select CalendarYear,monthnumber,sum(procedure_count) as procedure_count from [bi2].[BI_Analytics].[dbo].[Forecasting_44FZ_Fact]  group by CalendarYear,monthnumber order by CalendarYear,monthnumber;"))
-#подключаем остальные необходимые пакеты
+#РїРѕРґРєР»СЋС‡Р°РµРј РѕСЃС‚Р°Р»СЊРЅС‹Рµ РЅРµРѕР±С…РѕРґРёРјС‹Рµ РїР°РєРµС‚С‹
 
 library(lubridate)
 library(xts)
@@ -11,77 +12,77 @@ library(forecast)
 library(corrplot)
 library(zoo)
 
-#В первую очередь необходимо подобрать наиболее точную модель
+#Р’ РїРµСЂРІСѓСЋ РѕС‡РµСЂРµРґСЊ РЅРµРѕР±С…РѕРґРёРјРѕ РїРѕРґРѕР±СЂР°С‚СЊ РЅР°РёР±РѕР»РµРµ С‚РѕС‡РЅСѓСЋ РјРѕРґРµР»СЊ
 
-#записываем имеющиеся фактические данные для обучения и тестирования модели в объект TimeSeries
+#Р·Р°РїРёСЃС‹РІР°РµРј РёРјРµСЋС‰РёРµСЃСЏ С„Р°РєС‚РёС‡РµСЃРєРёРµ РґР°РЅРЅС‹Рµ РґР»СЏ РѕР±СѓС‡РµРЅРёСЏ Рё С‚РµСЃС‚РёСЂРѕРІР°РЅРёСЏ РјРѕРґРµР»Рё РІ РѕР±СЉРµРєС‚ TimeSeries
 TS_data <- ts(Groupped_Fact_Data_44FZ_by_month$procedure_count,start = c(2014,1), end = c(2018,10),frequency = 12)
 
-#Смотрим декомпозицию, чтоб понять, что из себя представляется наш временной ряд и какие свойства имеет
+#РЎРјРѕС‚СЂРёРј РґРµРєРѕРјРїРѕР·РёС†РёСЋ, С‡С‚РѕР± РїРѕРЅСЏС‚СЊ, С‡С‚Рѕ РёР· СЃРµР±СЏ РїСЂРµРґСЃС‚Р°РІР»СЏРµС‚СЃСЏ РЅР°С€ РІСЂРµРјРµРЅРЅРѕР№ СЂСЏРґ Рё РєР°РєРёРµ СЃРІРѕР№СЃС‚РІР° РёРјРµРµС‚
 plot(decompose(TS_data))
 
-#Выбираемся тренировочную выборку - 4 полных года
+#Р’С‹Р±РёСЂР°РµРјСЃСЏ С‚СЂРµРЅРёСЂРѕРІРѕС‡РЅСѓСЋ РІС‹Р±РѕСЂРєСѓ - 4 РїРѕР»РЅС‹С… РіРѕРґР°
 ts_training_set <- window(TS_data,start=c(2014,1),end=c(2017,12))
 
-#Обучаем разные модели
-auto_arima <- auto.arima(ts_training_set) #автоподбор ARIMA модели
-neural_net1 <- nnetar(ts_training_set,lambda=0) #Нейронная сеть с лябмда=0 (для положительного прогноза)
-neural_net2 <- nnetar(ts_training_set,lambda=1) #Нейронная сеть с лябмда=0 (для отрицательного прогноза)
-HW_Fit_add <- hw(ts_training_set,seasonal="additive") #Прогноз по методу экспоненциального сглаживания  Хольта - Винтерса с аддитивной сезонностью
-HW_Fit_multi <- hw(ts_training_set,seasonal="multiplicative") #Прогноз по методу экспоненциального сглаживания  Хольта - Винтерса с мультипликативной сезонностью
+#РћР±СѓС‡Р°РµРј СЂР°Р·РЅС‹Рµ РјРѕРґРµР»Рё
+auto_arima <- auto.arima(ts_training_set) #Р°РІС‚РѕРїРѕРґР±РѕСЂ ARIMA РјРѕРґРµР»Рё
+neural_net1 <- nnetar(ts_training_set,lambda=0) #РќРµР№СЂРѕРЅРЅР°СЏ СЃРµС‚СЊ СЃ Р»СЏР±РјРґР°=0 (РґР»СЏ РїРѕР»РѕР¶РёС‚РµР»СЊРЅРѕРіРѕ РїСЂРѕРіРЅРѕР·Р°)
+neural_net2 <- nnetar(ts_training_set,lambda=1) #РќРµР№СЂРѕРЅРЅР°СЏ СЃРµС‚СЊ СЃ Р»СЏР±РјРґР°=0 (РґР»СЏ РѕС‚СЂРёС†Р°С‚РµР»СЊРЅРѕРіРѕ РїСЂРѕРіРЅРѕР·Р°)
+HW_Fit_add <- hw(ts_training_set,seasonal="additive") #РџСЂРѕРіРЅРѕР· РїРѕ РјРµС‚РѕРґСѓ СЌРєСЃРїРѕРЅРµРЅС†РёР°Р»СЊРЅРѕРіРѕ СЃРіР»Р°Р¶РёРІР°РЅРёСЏ  РҐРѕР»СЊС‚Р° - Р’РёРЅС‚РµСЂСЃР° СЃ Р°РґРґРёС‚РёРІРЅРѕР№ СЃРµР·РѕРЅРЅРѕСЃС‚СЊСЋ
+HW_Fit_multi <- hw(ts_training_set,seasonal="multiplicative") #РџСЂРѕРіРЅРѕР· РїРѕ РјРµС‚РѕРґСѓ СЌРєСЃРїРѕРЅРµРЅС†РёР°Р»СЊРЅРѕРіРѕ СЃРіР»Р°Р¶РёРІР°РЅРёСЏ  РҐРѕР»СЊС‚Р° - Р’РёРЅС‚РµСЂСЃР° СЃ РјСѓР»СЊС‚РёРїР»РёРєР°С‚РёРІРЅРѕР№ СЃРµР·РѕРЅРЅРѕСЃС‚СЊСЋ
 
-#Построим графики и сравним визуально с фактом 10 месяцев 2018 года
+#РџРѕСЃС‚СЂРѕРёРј РіСЂР°С„РёРєРё Рё СЃСЂР°РІРЅРёРј РІРёР·СѓР°Р»СЊРЅРѕ СЃ С„Р°РєС‚РѕРј 10 РјРµСЃСЏС†РµРІ 2018 РіРѕРґР°
 
 autoplot(ts_p)+
   autolayer(forecast(neural_net1,h=10),series = "NeuralNet1",PI=FALSE) +
   autolayer(forecast(neural_net2,h=10),series = "NeuralNet2",PI=FALSE) +
   autolayer(forecast(HW_Fit_add,h=10),series = "HW_Fit_add",PI=FALSE) +
   autolayer(forecast(HW_Fit_multi,h=10),series = "HW_Fit_multi",PI=FALSE) +
-  autolayer(window(TS_data,start=c(2018,1),end=c(2018,10)),series = "Fact")  #факт 10 месяцев - test_set
+  autolayer(window(TS_data,start=c(2018,1),end=c(2018,10)),series = "Fact")  #С„Р°РєС‚ 10 РјРµСЃСЏС†РµРІ - test_set
 
-#Визуально, очевидно, лучше всего показали себя модели Хольта-Винтерса: "словили" тренд и сезонность. 
-#Чтобы точно определить наилучшую модель, вызовем summary, чтоб оценить критерий Акаике (AIC)
+#Р’РёР·СѓР°Р»СЊРЅРѕ, РѕС‡РµРІРёРґРЅРѕ, Р»СѓС‡С€Рµ РІСЃРµРіРѕ РїРѕРєР°Р·Р°Р»Рё СЃРµР±СЏ РјРѕРґРµР»Рё РҐРѕР»СЊС‚Р°-Р’РёРЅС‚РµСЂСЃР°: "СЃР»РѕРІРёР»Рё" С‚СЂРµРЅРґ Рё СЃРµР·РѕРЅРЅРѕСЃС‚СЊ. 
+#Р§С‚РѕР±С‹ С‚РѕС‡РЅРѕ РѕРїСЂРµРґРµР»РёС‚СЊ РЅР°РёР»СѓС‡С€СѓСЋ РјРѕРґРµР»СЊ, РІС‹Р·РѕРІРµРј summary, С‡С‚РѕР± РѕС†РµРЅРёС‚СЊ РєСЂРёС‚РµСЂРёР№ РђРєР°РёРєРµ (AIC)
 
 summary(HW_Fit_add) #AIC 1157   
 summary(HW_Fit_multi) #AIC 1241 
-#Выбираем модель экспоненциального сглаживания с трендом и сезонностью Хольта - Винтерса с аддитивной сезонностью!
+#Р’С‹Р±РёСЂР°РµРј РјРѕРґРµР»СЊ СЌРєСЃРїРѕРЅРµРЅС†РёР°Р»СЊРЅРѕРіРѕ СЃРіР»Р°Р¶РёРІР°РЅРёСЏ СЃ С‚СЂРµРЅРґРѕРј Рё СЃРµР·РѕРЅРЅРѕСЃС‚СЊСЋ РҐРѕР»СЊС‚Р° - Р’РёРЅС‚РµСЂСЃР° СЃ Р°РґРґРёС‚РёРІРЅРѕР№ СЃРµР·РѕРЅРЅРѕСЃС‚СЊСЋ!
 
 #####################################################################
 
-#Теперь приступаем к самому главному: созданию прогноза на 2 месяца 2018го и на весь 2019 год
+#РўРµРїРµСЂСЊ РїСЂРёСЃС‚СѓРїР°РµРј Рє СЃР°РјРѕРјСѓ РіР»Р°РІРЅРѕРјСѓ: СЃРѕР·РґР°РЅРёСЋ РїСЂРѕРіРЅРѕР·Р° РЅР° 2 РјРµСЃСЏС†Р° 2018РіРѕ Рё РЅР° РІРµСЃСЊ 2019 РіРѕРґ
 
-#Испопользовать будет подход bottom-up(снизу-вверх), 
-#т.к. согласно требованиям руководства, необходима некоторая детализация по измерениям (диапазон сумм закупок и участие только субъектов малого бизнеса).
-#очевидно данных подход не является оптимальным, но для написания функции, чтобы разбить на дамми-переменные и затем собрать воедино, потребовалось бы огромное количество времени
-#которого не было на тот момент
+#РСЃРїРѕРїРѕР»СЊР·РѕРІР°С‚СЊ Р±СѓРґРµС‚ РїРѕРґС…РѕРґ bottom-up(СЃРЅРёР·Сѓ-РІРІРµСЂС…), 
+#С‚.Рє. СЃРѕРіР»Р°СЃРЅРѕ С‚СЂРµР±РѕРІР°РЅРёСЏРј СЂСѓРєРѕРІРѕРґСЃС‚РІР°, РЅРµРѕР±С…РѕРґРёРјР° РЅРµРєРѕС‚РѕСЂР°СЏ РґРµС‚Р°Р»РёР·Р°С†РёСЏ РїРѕ РёР·РјРµСЂРµРЅРёСЏРј (РґРёР°РїР°Р·РѕРЅ СЃСѓРјРј Р·Р°РєСѓРїРѕРє Рё СѓС‡Р°СЃС‚РёРµ С‚РѕР»СЊРєРѕ СЃСѓР±СЉРµРєС‚РѕРІ РјР°Р»РѕРіРѕ Р±РёР·РЅРµСЃР°).
+#РѕС‡РµРІРёРґРЅРѕ РґР°РЅРЅС‹С… РїРѕРґС…РѕРґ РЅРµ СЏРІР»СЏРµС‚СЃСЏ РѕРїС‚РёРјР°Р»СЊРЅС‹Рј, РЅРѕ РґР»СЏ РЅР°РїРёСЃР°РЅРёСЏ С„СѓРЅРєС†РёРё, С‡С‚РѕР±С‹ СЂР°Р·Р±РёС‚СЊ РЅР° РґР°РјРјРё-РїРµСЂРµРјРµРЅРЅС‹Рµ Рё Р·Р°С‚РµРј СЃРѕР±СЂР°С‚СЊ РІРѕРµРґРёРЅРѕ, РїРѕС‚СЂРµР±РѕРІР°Р»РѕСЃСЊ Р±С‹ РѕРіСЂРѕРјРЅРѕРµ РєРѕР»РёС‡РµСЃС‚РІРѕ РІСЂРµРјРµРЅРё
+#РєРѕС‚РѕСЂРѕРіРѕ РЅРµ Р±С‹Р»Рѕ РЅР° С‚РѕС‚ РјРѕРјРµРЅС‚
 
-#Набираем датасеты, где первая цифра в названии - номер диапазона, вторая - МСП или нет
+#РќР°Р±РёСЂР°РµРј РґР°С‚Р°СЃРµС‚С‹, РіРґРµ РїРµСЂРІР°СЏ С†РёС„СЂР° РІ РЅР°Р·РІР°РЅРёРё - РЅРѕРјРµСЂ РґРёР°РїР°Р·РѕРЅР°, РІС‚РѕСЂР°СЏ - РњРЎРџ РёР»Рё РЅРµС‚
 train_data_1_0 <- sqlQuery(dbconnection,paste("select sum(Procedure_Count) as  Procedure_Count from [bi2].[BI_Analytics].[dbo].[Forecasting_44FZ_Fact_for_modeling] where DocPublishDate<='20181031' and Sum_range_Order=1 and IS_MSP_Purchase=0 group by year(DocPublishDate),MonthNumber order by year(DocPublishDate),MonthNumber;"))
 train_data_2_0 <- sqlQuery(dbconnection,paste("select sum(Procedure_Count) as  Procedure_Count from [bi2].[BI_Analytics].[dbo].[Forecasting_44FZ_Fact_for_modeling] where DocPublishDate<='20181031' and Sum_range_Order=2 and IS_MSP_Purchase=0 group by year(DocPublishDate),MonthNumber order by year(DocPublishDate),MonthNumber;"))
 train_data_3_0 <- sqlQuery(dbconnection,paste("select sum(Procedure_Count) as  Procedure_Count from [bi2].[BI_Analytics].[dbo].[Forecasting_44FZ_Fact_for_modeling] where DocPublishDate<='20181031' and Sum_range_Order=3 and IS_MSP_Purchase=0 group by year(DocPublishDate),MonthNumber order by year(DocPublishDate),MonthNumber;"))
 train_data_1_1 <- sqlQuery(dbconnection,paste("select sum(Procedure_Count) as  Procedure_Count from [bi2].[BI_Analytics].[dbo].[Forecasting_44FZ_Fact_for_modeling] where DocPublishDate<='20181031' and Sum_range_Order=1 and IS_MSP_Purchase=1 group by year(DocPublishDate),MonthNumber order by year(DocPublishDate),MonthNumber;"))
 train_data_2_1 <- sqlQuery(dbconnection,paste("select sum(Procedure_Count) as  Procedure_Count from [bi2].[BI_Analytics].[dbo].[Forecasting_44FZ_Fact_for_modeling] where DocPublishDate<='20181031' and Sum_range_Order=2 and IS_MSP_Purchase=1 group by year(DocPublishDate),MonthNumber order by year(DocPublishDate),MonthNumber;"))
 train_data_3_1 <- sqlQuery(dbconnection,paste("select sum(Procedure_Count) as  Procedure_Count from [bi2].[BI_Analytics].[dbo].[Forecasting_44FZ_Fact_for_modeling] where DocPublishDate<='20181031' and Sum_range_Order=3 and IS_MSP_Purchase=1 group by year(DocPublishDate),MonthNumber order by year(DocPublishDate),MonthNumber;"))
-#Создаем тренировочные временные ряды из мини-сетов
+#РЎРѕР·РґР°РµРј С‚СЂРµРЅРёСЂРѕРІРѕС‡РЅС‹Рµ РІСЂРµРјРµРЅРЅС‹Рµ СЂСЏРґС‹ РёР· РјРёРЅРё-СЃРµС‚РѕРІ
 ts_1_0 <- zooreg(train_data_1_0,start = as.yearmon("2014-01"),end = as.yearmon("2018-10"),freq=12)
 ts_2_0 <- zooreg(train_data_2_0,start = as.yearmon("2014-01"),end = as.yearmon("2018-10"),freq=12)
 ts_3_0 <- zooreg(train_data_3_0,start = as.yearmon("2014-01"),end = as.yearmon("2018-10"),freq=12)
 ts_1_1 <- zooreg(train_data_1_1,start = as.yearmon("2014-01"),end = as.yearmon("2018-10"),freq=12)
 ts_2_1 <- zooreg(train_data_2_1,start = as.yearmon("2014-01"),end = as.yearmon("2018-10"),freq=12)
 ts_3_1 <- zooreg(train_data_3_1,start = as.yearmon("2014-01"),end = as.yearmon("2018-10"),freq=12)
-#"скармливаем"  эти ряды моделям - обучаем
+#"СЃРєР°СЂРјР»РёРІР°РµРј"  СЌС‚Рё СЂСЏРґС‹ РјРѕРґРµР»СЏРј - РѕР±СѓС‡Р°РµРј
 HW_Fit_1_0 <- hw(ts_1_0,seasonal="additive")
 HW_Fit_2_0 <- hw(ts_2_0,seasonal="additive")
 HW_Fit_3_0 <- hw(ts_3_0,seasonal="additive")
 HW_Fit_1_1 <- hw(ts_1_1,seasonal="additive")   
 HW_Fit_2_1 <- hw(ts_2_1,seasonal="additive") 
 HW_Fit_3_1 <- hw(ts_3_1,seasonal="additive") 
-#Прогнозируем
+#РџСЂРѕРіРЅРѕР·РёСЂСѓРµРј
 prognoz_1_0<-forecast(HW_Fit_1_0,h=14) 
 prognoz_2_0<-forecast(HW_Fit_2_0,h=14)
 prognoz_3_0<-forecast(HW_Fit_3_0,h=14)
 prognoz_1_1<-forecast(HW_Fit_1_1,h=14)
 prognoz_2_1<-forecast(HW_Fit_2_1,h=14) 
 prognoz_3_1<-forecast(HW_Fit_3_1,h=14)
-#Собираем результаты в эксель для дальнейшей обработки
+#РЎРѕР±РёСЂР°РµРј СЂРµР·СѓР»СЊС‚Р°С‚С‹ РІ СЌРєСЃРµР»СЊ РґР»СЏ РґР°Р»СЊРЅРµР№С€РµР№ РѕР±СЂР°Р±РѕС‚РєРё
 Forecast_2019_Count<-data.frame(prognoz_1_0$mean,prognoz_2_0$mean,prognoz_3_0$mean,prognoz_1_1$mean,prognoz_2_1$mean,prognoz_3_1$mean)
 write.xlsx(Forecast_2019_Count, "Forecast_2019_Count.xlsx", col_names = TRUE)
